74e69c4bee26c3e645890fbf896933611902d4d4
johnlyden <john.lyden6@gmail.com>
2015-10-14 13:10:51 -0400

    adding associations to taggings and opportunity models

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,2 +1,5 @@
 class Opportunity < ActiveRecord::Base
+  has_many :taggings
+  has_many :tags, through: :taggings
+  belongs_to :organization
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+  has_many :taggings
+  has_manny :opportunities, through: :taggings
 end

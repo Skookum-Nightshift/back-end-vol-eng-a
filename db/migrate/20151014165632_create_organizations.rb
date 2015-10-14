2c2addd347c7716d880eb9b9d7d4edf63fe7a4dc
johnlyden <john.lyden6@gmail.com>
2015-10-14 13:02:37 -0400

    adding category and organization migration

--- db/migrate/20151014165632_create_organizations.rb
+++ db/migrate/20151014165632_create_organizations.rb
@@ -1,6 +1,15 @@
 class CreateOrganizations < ActiveRecord::Migration
   def change
     create_table :organizations do |t|
+      t.string :name
+      t.string :address
+      t.string :email
+      t.string :web
+      t.string :volunteer_url
+      t.string :facebook
+      t.string :twitter
+      t.string :youtube
+      t.text :description
 
       t.timestamps null: false
     end

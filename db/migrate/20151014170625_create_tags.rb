6f542717244aaaa1fa694280da97ca03da71404e
johnlyden <john.lyden6@gmail.com>
2015-10-14 13:08:53 -0400

    adding tag and tagging migration

--- db/migrate/20151014170625_create_tags.rb
+++ db/migrate/20151014170625_create_tags.rb
@@ -1,6 +1,7 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
+      t.string :name
 
       t.timestamps null: false
     end
--- db/migrate/20151014170633_create_taggings.rb
+++ db/migrate/20151014170633_create_taggings.rb
@@ -1,6 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :taggings do |t|
+      t.belongs_to :opportunity
+      t.belongs_to :tag
 
       t.timestamps null: false
     end

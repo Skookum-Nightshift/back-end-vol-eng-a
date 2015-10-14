522f4130ffef58db40ffd92f3ffb0b0a9de2930d
johnlyden <john.lyden6@gmail.com>
2015-10-14 13:03:35 -0400

    adding categorizations migration"

--- db/migrate/20151014165647_create_categorizations.rb
+++ db/migrate/20151014165647_create_categorizations.rb
@@ -1,6 +1,8 @@
 class CreateCategorizations < ActiveRecord::Migration
   def change
     create_table :categorizations do |t|
+      t.belongs_to :organization
+      t.belongs_to :category
 
       t.timestamps null: false
     end

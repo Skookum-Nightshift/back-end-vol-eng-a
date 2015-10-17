065f6b79338f03e6ae6f181e44c192ea7366b9c8
johnlyden <john.lyden6@gmail.com>
2015-10-16 22:22:15 -0400

    adding org show action

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -3,4 +3,9 @@ class Api::OrganizationsController < ApplicationController
     @organizations = Organization.all
     render json: @organizations
   end
+
+  def show
+    @organization = Organization.find(params[:id])
+    render json: @organization
+  end
 end

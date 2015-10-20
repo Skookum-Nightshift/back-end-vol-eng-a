30b8f59e0f1db6fb553072420a9b7b5653674433
johnlyden <john.lyden6@gmail.com>
2015-10-20 19:55:06 -0400

    adding find_opps method on opportunity model

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,4 +1,8 @@
 class Opportunity < ActiveRecord::Base
   acts_as_taggable_on :tags
   belongs_to :organization
+
+  def find_opps(tag_ary)
+    Opportunity.tagged_with(tag_ary, :match_all => true)
+  end
 end

f3285a842e1a7394a1c0f947651495d1a4a8a25a
johnlyden <john.lyden86@gmail.com>
2015-10-21 15:48:45 -0400

    moving controllers into api/v1 folder and namespacing

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,4 +1,4 @@
-class Api::CategoriesController < ApplicationController
+class Api::V1::CategoriesController < ApplicationController
   def index
     @categories = Category.all
     render json: @categories

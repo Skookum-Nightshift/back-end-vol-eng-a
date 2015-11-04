7fca4e3778edf076d85d0a4577f92482ba6c8a31
johnlyden <john.lyden86@gmail.com>
2015-11-04 17:53:26 -0500

    fixed seed file for seeding zip code

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'json'
 data = JSON.parse(File.read('db/fixtures/orgs.json'))
 
 data.each do |org|
-  x = Organization.create(name: org["name"], address: org["contact"]["address"], city: org["contact"]["city"], state: org["contact"]["state"], zip: ["contact"]["zip"], email: org["contact"]["email"], web: org["contact"]["web"], volunteer_url: org["contact"]["volunteer_url"], facebook: org["contact"]["facebook"], twitter: org["contact"]["twitter"], youtube: org["contact"]["youtube"], description: org["description"])
+  x = Organization.create(name: org["name"], address: org["contact"]["address"], city: org["contact"]["city"], state: org["contact"]["state"], zip: org["contact"]["zip"], email: org["contact"]["email"], web: org["contact"]["web"], volunteer_url: org["contact"]["volunteer_url"], facebook: org["contact"]["facebook"], twitter: org["contact"]["twitter"], youtube: org["contact"]["youtube"], description: org["description"])
 
   org["categories"].each do |category|
     x.categories << Category.find_or_create_by(name: category)

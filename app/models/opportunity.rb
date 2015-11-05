21fe667ce2111a5a5ed722901ac18de4c010754a
Jonathan Gertig <jcgertig@gmail.com>
2015-11-05 18:52:47 -0500

    Update opportunity.rb
    first 3

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -3,6 +3,6 @@ class Opportunity < ActiveRecord::Base
   belongs_to :organization
 
   def self.find_opps(tag_ary)
-    Opportunity.tagged_with(tag_ary, any: true)
+    Opportunity.tagged_with(tag_ary, any: true).first(3)
   end
 end

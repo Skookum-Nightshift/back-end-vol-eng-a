6c9a944b8b3c978faac5e38cdd57ac0af75fff94
KileyDowling <kileydowling@gmail.com>
2015-11-07 00:04:38 -0500

    updated find_opps

---
+++ app/admin/opportunity.rb
@@ -0,0 +1,33 @@
+ActiveAdmin.register Opportunity do
+
+
+form do |f|
+  f.semantic_errors *f.object.errors.keys
+  f.inputs 'Details' do
+    f.input :organization
+    f.input :name
+    f.input :address
+    f.input :description
+    f.input :tag_list, :as => :select,
+                             :multiple => :true,
+                             :collection => ActsAsTaggableOn::Tag.all
+  end
+  f.actions
+end
+
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end

dfcb8f3b6e9d277f7616f40b674dd0b3d9c897d2
johnlyden <john.lyden86@gmail.com>
2015-11-14 20:31:56 -0500

    connection being made and saved to database

--- app/controllers/api/v1/connections_controller.rb
+++ app/controllers/api/v1/connections_controller.rb
@@ -1,2 +1,12 @@
-class API::V1::ConnectionsController < ApplicationController
+class Api::V1::ConnectionsController < API::V1::BaseController
+  def create
+    @connection = Connection.create(connection_params)
+    render json: @connection
+  end
+
+  private
+
+  def connection_params
+    params.require(:connection).permit(:firstname, :lastname, :email, :zipcode, :opportunity_id, :organization_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020221628) do
+ActiveRecord::Schema.define(version: 20151115005720) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 20151020221628) do
   create_table "categorizations", force: :cascade do |t|
     t.integer  "organization_id"
     t.integer  "category_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "connections", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "zipcode"
+    t.integer  "opportunity_id"
+    t.integer  "organization_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end

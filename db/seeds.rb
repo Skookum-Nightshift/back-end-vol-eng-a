03b90967216a667cd4f25734702118cd75975e47
johnlyden <john.lyden86@gmail.com>
2015-11-16 00:21:26 -0500

    downcasing all tags and cateogires

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ data.each do |org|
   x = Organization.create(name: org["name"], address: org["contact"]["address"], city: org["contact"]["city"], state: org["contact"]["state"], zip: org["contact"]["zip"], email: org["contact"]["email"], web: org["contact"]["web"], volunteer_url: org["contact"]["volunteer_url"], facebook: org["contact"]["facebook"], twitter: org["contact"]["twitter"], youtube: org["contact"]["youtube"], description: org["description"])
 
   org["categories"].each do |category|
-    x.categories << Category.find_or_create_by(name: category)
+    x.categories << Category.find_or_create_by(name: category.downcase)
   end
 
   org["opportunities"].each do |opportunity|
     y = Opportunity.create(name: opportunity["name"], address: opportunity["address"], description: opportunity["description"])
     opportunity["tags"].each do |tag|
-      y.tag_list.add(tag)
+      y.tag_list.add(tag.downcase)
     end
     x.opportunities << y
   end

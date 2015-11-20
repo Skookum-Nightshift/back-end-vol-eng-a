17f172ea150d538f8bd612743c56e4b4b1363553
johnlyden <john.lyden86@gmail.com>
2015-11-19 23:52:02 -0500

    adding logo attribute to serializer

--- app/serializers/api/v1/organization_serializer.rb
+++ app/serializers/api/v1/organization_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::OrganizationSerializer < ActiveModel::Serializer
 
-  attributes :organization_id, :name, :address, :state, :city, :zip, :description, :web, :volunteer_url, :facebook, :twitter, :youtube, :categories, :opportunities
+  attributes :organization_id, :name, :address, :state, :city, :zip, :description, :web, :volunteer_url, :facebook, :twitter, :youtube, :categories, :opportunities, :logo
 
   def organization_id
     object.id

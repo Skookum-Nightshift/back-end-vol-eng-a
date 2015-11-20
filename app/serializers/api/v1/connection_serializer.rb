425b1902f91521594dbfc68e1bf6778c974f8d52
johnlyden <john.lyden86@gmail.com>
2015-11-20 11:23:14 -0500

    adding org_email to serailzer

--- app/serializers/api/v1/connection_serializer.rb
+++ app/serializers/api/v1/connection_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ConnectionSerializer < ActiveModel::Serializer
-  attributes :firstname, :lastname, :email, :zipcode, :opportunity_id, :organization_id, :opportunity_name, :organization_name
+  attributes :firstname, :lastname, :email, :zipcode, :opportunity_id, :organization_id, :opportunity_name, :organization_name, :organization_email
 
   def connection_id
     object.id
